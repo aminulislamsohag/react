3dc7bb4d86ed3eeca88fb56614a817f6ec76f643
aminulislamsohag <38567911+aminulislamsohag@users.noreply.github.com>
2024-04-30 01:30:58 +0600

    Initial commit

---
+++ src/main/java/com/react/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.react.repository;
+
+import com.react.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+}
